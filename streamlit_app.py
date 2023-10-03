c2a5373b55fbc963de833c32c6512274c479fd51
Paran <111531399+Paranthaman2k@users.noreply.github.com>
2023-10-03 17:22:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import streamlit_analytics
 
+
+with streamlit_analytics.track():
+	st.text_input("Paranthaman")
+	st.selectbox("select your favorite", ["guvi","data","science"])
+	st.button("click me")
